a1155631fdb27892f721c4e029c2d4174ed74bf8
Jaimin Rathod <jaiminrathod9865@gmail.com>
2025-09-01 00:30:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function performConversion() {
   const from = parseInt(fromBase.value);
   const to = parseInt(toBase.value);
   if (!validate(value, from)) {
-    alert('Number to daal bhai! ' + from);
+    alert('😏 Number to daal bhai! ' + from);
     return;
   }
   const decimal = parseInt(value, from);
